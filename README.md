130ae1841e60693aa1408c17b80d254120fdfaea
Ra-m-ha <dram1008@yandex.ru>
2020-01-28 11:41:42 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # yii2-widget-kolada-dar1
+
 Виджет для Yii2 для рисования календаря Коляда Дар на лето
+
+Версия 1.0.0
+
+Выводит месяца $monthArray в табличный календарь
+визуально это выглядит так:
+месяца по 41 день справа месяца по 40 дней слева
+всего таких строк 5, последняя соответственно с одним месяцем
+
+в данных $monthArray:
+эти месяца должны быть под индексами от 1 до 9
+в месяце содержатся строки - недели так же это массив с индексами от 1 до 9
+в неделе содержатся 5 дней (клеточка-столбик) с индексами от 1 до 6
+
+![](images/2020-01-28_11-17-51.png)
+
+## Пример использования
+
+```
+$monthArray = \avatar\widgets\KaladaDar1::getMonthArray(9, false);
+echo \avatar\widgets\KaladaDar1::widget([
+    'monthArray'  => $monthArray,
+    'optionsWeek' => [
+         1 => ['style' => 'background-color: #000000; color: #ffffff;'],
+         2 => ['style' => 'background-color: #ffa6a6;'],
+         3 => ['style' => 'background-color: #ffd2a6;'],
+         4 => ['style' => 'background-color: #ffffa6;'],
+         5 => ['style' => 'background-color: #a7fca4;'],
+         6 => ['style' => 'background-color: #a6a6ff;'],
+         7 => ['style' => 'background-color: #bda5d1;'],
+         8 => ['style' => 'background-color: #d4a6f7;'],
+         9 => ['style' => 'background-color: #ffffff;'],
+     ],
+    'optionsColumn' => [
+         1 => ['style' => 'width: 32px;'],
+         2 => ['style' => 'width: 32px;'],
+         3 => ['style' => 'width: 32px;'],
+         4 => ['style' => 'width: 32px;'],
+         5 => ['style' => 'width: 32px;'],
+         6 => ['style' => 'width: 32px;'],
+     ],
+]); 
+```
+
+`optionsWeek` - массив опций для тега `tr` для каждой недели, индексы могут быть от 1 до 9
+
+`optionsColumn` - массив опций для тега `th` для каждой колонки месяца, индексы могут быть от 1 до 6
+
+`monthArray` - массив месяцев, индексы от 1 до 9. Месяц это массив недель от 1 до 9. Неделя это массив дней от 1 до 6. День содержист число или строку обозначающую порядковый номер месяца
---
+++ src/KaladaDar1.php
@@ -0,0 +1,254 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: s.arhangelskiy
+ * Date: 27.09.2016
+ * Time: 17:24
+ */
+
+namespace iAvatar777\widgets;
+
+use Yii;
+use yii\base\Widget;
+use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+
+/**
+ * Class KaladaDar1
+ * @package avatar\widgets
+ *
+ * Выводит месяца $monthArray в табличный календарь
+ * визуально это выглядит так:
+ * месяца по 41 день справа месяца по 40 дней слева
+ * всего таких строк 5, последняя соответственно с одним месяцем
+ *
+ * в данных $monthArray:
+ * эти месяца должны быть под индексами от 1 до 9
+ * в месяце содержатся строки - недели так же это массив с индексами от 1 до 9
+ * в неделе содержатся 5 дней (клеточка-столбик) с индексами от 1 до 6
+ */
+class KaladaDar1 extends Widget
+{
+    public $monthArray;
+
+    public $optionsWeek = [
+        1 => ['style' => 'background-color: #000000; color: #ffffff;'],
+        2 => ['style' => 'background-color: #ffa6a6;'],
+        3 => ['style' => 'background-color: #ffd2a6;'],
+        4 => ['style' => 'background-color: #ffffa6;'],
+        5 => ['style' => 'background-color: #a7fca4;'],
+        6 => ['style' => 'background-color: #a6a6ff;'],
+        7 => ['style' => 'background-color: #bda5d1;'],
+        8 => ['style' => 'background-color: #d4a6f7;'],
+        9 => ['style' => 'background-color: #ffffff;'],
+    ];
+
+    public $optionsColumn = [
+        1 => ['style' => 'width: 32px;'],
+        2 => ['style' => 'width: 32px;'],
+        3 => ['style' => 'width: 32px;'],
+        4 => ['style' => 'width: 32px;'],
+        5 => ['style' => 'width: 32px;'],
+        6 => ['style' => 'width: 32px;'],
+    ];
+
+    public function init()
+    {
+        parent::init();
+        ob_start();
+    }
+
+    public function run()
+    {
+        $content = ob_get_clean();
+        $head = $this->head();
+        $body = $this->body();
+
+        return Html::tag(
+            'table',
+            $head . $body,
+            [
+                'class' => 'table table-hover table-striped'
+            ]
+            );
+    }
+
+    private function head()
+    {
+        $headers = [
+            ['name' => '#'],
+            ['name' => 'Название'],
+            ['name' => '1', 'options' => $this->optionsColumn[1]],
+            ['name' => '2', 'options' => $this->optionsColumn[2]],
+            ['name' => '3', 'options' => $this->optionsColumn[3]],
+            ['name' => '4', 'options' => $this->optionsColumn[4]],
+            ['name' => '5', 'options' => $this->optionsColumn[5]],
+            ['name' => '6', 'options' => $this->optionsColumn[6]],
+            ['name' => '1', 'options' => $this->optionsColumn[1]],
+            ['name' => '2', 'options' => $this->optionsColumn[2]],
+            ['name' => '3', 'options' => $this->optionsColumn[3]],
+            ['name' => '4', 'options' => $this->optionsColumn[4]],
+            ['name' => '5', 'options' => $this->optionsColumn[5]],
+            ['name' => '6', 'options' => $this->optionsColumn[6]],
+        ];
+        $rows = [];
+        foreach ($headers as $h) {
+            $options = [];
+            if (isset($h['options'])) {
+                $options = $h['options'];
+            }
+            $rows[] = Html::tag('th', $h['name'], $options);
+        }
+        $head = Html::tag(
+            'thead',
+            join('', $rows),
+            []
+        );
+
+        return $head;
+    }
+
+    private function body()
+    {
+        // заполняю пустые месяцы
+        $monthArray = $this->monthArray;
+
+        $rowsCount = 5;
+        $week = 9;
+        $cols = 2;
+        $weekDays = [
+            1 => 'Понедельникъ',
+            2 => 'Вторникъ',
+            3 => 'Третейникъ',
+            4 => 'Четверикъ',
+            5 => 'Пятница',
+            6 => 'Шестица',
+            7 => 'Седьмица',
+            8 => 'Осьмица',
+            9 => 'Неделя',
+        ];
+        $rows5 = [];
+        /** @var int $r  строка месяцев в календаре */
+        for($r = 1; $r <= $rowsCount; $r++) {
+            $rows9 = [];
+            for($i = 1; $i <= 9; $i++) {
+                $row = [];
+                $row[0] = Html::tag('td', $i);
+                $row[1] = Html::tag('td', $weekDays[$i]);
+                for($j = 1; $j <= 6; $j++) {
+                    $row[$j + 1] = Html::tag('td', $monthArray[($r-1)*2 + 1][$i][$j]);
+                }
+                if ($r < $rowsCount) {
+                    for($j = 1; $j <= 6; $j++) {
+                        $row[$j + 1 + 6] = Html::tag('td', $monthArray[($r-1)*2 + 2][$i][$j]);
+                    }
+                } else {
+                    for($j = 1; $j <= 6; $j++) {
+                        $row[$j + 1 + 6] = Html::tag('td', '');
+                    }
+                }
+                $rows9[$i] = $row;
+            }
+            $rows5[$r] = $rows9;
+        }
+
+        $d5 = [];
+        foreach ($rows5 as $r1) {
+            $r = [];
+            for ($g = 1; $g <= 9; $g++) {
+                $tr = $r1[$g];
+                $r[] = Html::tag('tr', join('', $tr), $this->optionsWeek[$g]);
+            }
+            $d5[] =  join('', $r);
+        }
+        $body = Html::tag(
+            'tbody',
+            join('', $d5),
+            []
+        );
+
+        return $body;
+    }
+
+    /**
+     * Генерирует массив месяцев для значения $this->monthArray
+     *
+     * @param int $day день недели с которого начинается год от 1 до 9
+     * @param bool $isSacral флаг. Это священный год? Если да то все месяца будут по 41 дню
+     * @return array
+     * эти месяца должны быть под индексами от 1 до 9
+     * в месяце содержатся строки - недели так же это массив с индексами от 1 до 9
+     * в неделе содержатся 5 дней (клеточка-столбик) с индексами от 1 до 6
+     */
+    public static function getMonthArray($day, $isSacral = false)
+    {
+        $m = [];
+        $d = $day; // день недели с которого начинается месяц от 1 до 9
+        for($r = 1; $r <= 9; $r++) {
+            $count = ($r % 2 == 1) ? 41 : ($isSacral? 41 : 40);
+            $m[$r] = self::getMonth($d, $count);
+            $d = (($count + ($d-1)) % 9) + 1;
+        }
+
+        return $m;
+    }
+
+    /**
+     * Генерирует месяц для значения $this->monthArray
+     *
+     * @param int $day день недели с которого начинается год от 1 до 9
+     * @param int $count кол-во дней в месяце
+     * @return array
+     * в месяце содержатся строки - недели так же это массив с индексами от 1 до 9
+     * в неделе содержатся 5 дней (клеточка-столбик) с индексами от 1 до 6
+     */
+    public static function getMonth($day, $count)
+    {
+        $out = [];
+        $add = ($count == 40)? 0 : 1;
+        // заполнение пустых полей
+        for ($r = 1; $r < $day; $r++) {
+            $out[$r] = [ 1 => ''];
+        }
+        $d = 1; // порядковый день в месяце
+        for ($r = $day; $r <= 9; $r++) {
+            $out[$r] = [ 1 => $d];
+            $d++;
+        }
+        $f = 3; // полных недель в графике по мимо первой
+        $isLast = true; // флаг будет ли еще последняя обрезанная неделя?
+        if ($day == 6 - $add) {
+            $f = 4;
+            $isLast = false;
+        }
+        if ($day > 6 - $add) {
+            $f = 4;
+        }
+        for ($l = 1; $l <= $f; $l++) {
+            for ($r = 1; $r <= 9; $r++) {
+                $out[$r][1 + $l] = $d;
+                $d++;
+            }
+        }
+        if ($isLast) {
+            $g = $count - (9*$f) - (9 - ($day - 1)); // сколько дней осталось в месяце в последнюю неделю
+            for ($r = 1; $r <= $g; $r++) {
+                $out[$r][$f+2] = $d;
+                $d++;
+            }
+            for ($r = $g + 1; $r <= 9; $r++) {
+                $out[$r][$f+2] = '';
+            }
+        }
+        if (!($f == 4 and $isLast == true)) {
+            for ($r = 1; $r <= 9; $r++) {
+                $out[$r][6] = '';
+            }
+        }
+
+        return $out;
+    }
+}
